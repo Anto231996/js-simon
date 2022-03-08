c8e9e632955e1d47396addae8d32fff86510a227
Anto231996 <tony2396@hotmail.it>
2022-03-08 15:30:35 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+/*  
+    Visualizzare in pagina 5 numeri casuali.
+    Da lì parte un timer di 30 secondi.
+    Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+const arrayNumberRandom = []
+
+console.log(arrayNumberRandom)
+
+
+
+for(let i = 0; i < 5; i++){
+    randomNum = Math.floor(Math.random()* 100) + 1;
+    document.getElementById("numeri-casuali").innerHTML += (randomNum + " ");
+    arrayNumberRandom.push(randomNum); 
+}
+
+
+
+setTimeout(deleteNumber, 5000);
+
+function deleteNumber(){
+    document.getElementById("numeri-casuali").innerHTML = "";
+}
+
+clearTimeout(deleteNumber);
+
+
+
+setTimeout(myTimeout, 5000);
+
+function myTimeout(){
+    for(let i = 0; i < 5; i++){
+        let userNum = parseInt(prompt("inserisci un numero")) 
+        console.log(userNum)
+    }
+};
+
+clearTimeout(myTimeout);
